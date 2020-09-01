2398d6fbeab6713bd09d8da5a7fe04a59d38f005
Zun <zunsamuel97@gmail.com>
2020-08-31 19:10:15 -0500

    step 166

--- recipes/src/main/java/com/zun/recipes/domain/Ingredient.java
+++ recipes/src/main/java/com/zun/recipes/domain/Ingredient.java
@@ -1,8 +1,13 @@
 package com.zun.recipes.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
+@NoArgsConstructor
 @Entity
 public class Ingredient {
 
@@ -25,43 +30,4 @@ public class Ingredient {
         this.uom = uom;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure uom) {
-        this.uom = uom;
-    }
 }
--- recipes/src/main/java/com/zun/recipes/services/RecipeServiceImpl.java
+++ recipes/src/main/java/com/zun/recipes/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.zun.recipes.services;
 
 import com.zun.recipes.domain.Recipe;
 import com.zun.recipes.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,10 +20,10 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+
         Set<Recipe> toReturn = new HashSet<>();
-
         recipeRepository.findAll().iterator().forEachRemaining(toReturn::add);
-
         return toReturn;
     }
 }
